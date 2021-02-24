6d464e76fbb406e50e14b0d05567808bfeedfd89
jinMac <jinhyun0504@hanmail.net>
2021-02-24 22:31:05 +0900

    EnableHystrix

--- recruitment/src/main/java/getjob/RecruitmentApplication.java
+++ recruitment/src/main/java/getjob/RecruitmentApplication.java
@@ -1,15 +1,18 @@
 package getjob;
-import getjob.config.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.ApplicationContext;
+
+import getjob.config.kafka.KafkaProcessor;
 
 
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
+@EnableHystrix
 public class RecruitmentApplication {
     protected static ApplicationContext applicationContext;
     public static void main(String[] args) {
